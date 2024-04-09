5bb2cfcc362062020eef81102cbde82e310056b4
Lev Rednikov <120374210+CheateRYT@users.noreply.github.com>
2024-04-09 19:42:43 +0300

    addTodo reducer and all todoSlice.actions

--- src/store/todoSlice.js
+++ src/store/todoSlice.js
@@ -3,5 +3,19 @@ import { createSlice } from "@reduxjs/toolkit";
 const todoSlice = createSlice({
   name: "todo",
   initialState: { todos: [] },
-  reducers: {},
+  reducers: {
+    addTodo(state, action) {
+      console.log(state);
+      console.log(action);
+      state.todos.push({
+        id: new Date().toISOString(),
+        text: action.payload.text,
+        completed: false,
+      });
+    },
+    removeTodo(state, action) {},
+    toggleTodoComplete(state, action) {},
+  },
 });
+
+export const { addTodo, removeTodo, toggleTodoComplete } = todoSlice.actions;
